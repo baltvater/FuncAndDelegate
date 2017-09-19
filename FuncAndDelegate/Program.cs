51363a268ed1c15a097825e285c1ea45ab81059c
Bill <baltvater@comcast.net>
2017-09-19 15:34:31 -0500

    separate methods

--- FuncAndDelegate/Program.cs
+++ FuncAndDelegate/Program.cs
@@ -2,29 +2,54 @@
 
 namespace FuncAndDelegate
 {
-    class Program
+    public class Program
     {
         delegate string[] ExtractMethod(string title, int limit);
+        static string title = "The Scarlet Letter";
 
         static void Main()
         {
-            ExtractMethod extractMethod = ExtractWords;
-            string title = "The Scarlet Letter";
-            foreach (string word in extractMethod(title, 5))
-            {
-                Console.WriteLine(word);
-            }
+            ExplicitlyDefineADelegate();
+            InstantiateAFuncDelegate();
 
             Console.Read();
         }
 
-        private static string[] ExtractWords(string title, int limit)
+        static void ExplicitlyDefineADelegate()
+        {
+            ExtractMethod extractMethod = ExtractWords;
+            WriteItOut(extractMethod);
+        }
+
+        static void InstantiateAFuncDelegate()
+        {
+            Func<string, int, string[]> extractMethod = ExtractWords;
+            WriteItOut(extractMethod);
+        }
+
+        static string[] ExtractWords(string title, int limit)
         {
             char[] delimiters = new char[] { ' ' };
             if (limit > 0)
                 return title.Split(delimiters, limit);
             else
                 return title.Split(delimiters);
+        }
+
+        static void WriteItOut(ExtractMethod extractMethod)
+        {
+            foreach (string word in extractMethod(title, 5))
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void WriteItOut(Func<string, int, string[]> extractMethod)
+        {
+            foreach (string word in extractMethod(title, 5))
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
